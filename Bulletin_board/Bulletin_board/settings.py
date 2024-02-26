c62b0fd46306486e6ea2aa59f6cdc586bc3ec9a8
Nikolay Baskakov <bnn300702@yandex.ru>
2024-02-26 18:24:07 +0300

    Create models in "accounts" and "Main_board"; update settings.py

--- Bulletin_board/Bulletin_board/settings.py
+++ Bulletin_board/Bulletin_board/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Main_board',
+    'accounts',
     'django_summernote'
 ]
 
@@ -133,3 +134,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+AUTH_USER_MODEL = 'accounts.User'
--- Bulletin_board/Main_board/models.py
+++ Bulletin_board/Main_board/models.py
@@ -1,3 +1,56 @@
 from django.db import models
-
+from accounts.models import User
 # Create your models here.
+class Post(models.Model):
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+    title = models.CharField(max_length=25, verbose_name='Заголовок')
+    creation_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата Создания')
+    edit_date = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор')
+    text = models.TextField()
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, verbose_name='Категория')
+    
+    def __str__(self):
+        return str(self.pk)
+    
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+    TANK = 'TK'
+    HEALER = 'HL'
+    DAMAGE_DEALER = 'DD'
+    TRADER = 'TD'
+    GUILDMASTER = 'GM'
+    QUESTGIVER = 'QG'
+    BLACKSMITH = 'BS'
+    TANNER = 'TN'
+    POTION_MAKER = 'PM'
+    SPELL_MASTER = 'SM'
+    TYPES = [
+        (TANK, 'Танки'),
+        (HEALER, 'Хилы'),
+        (DAMAGE_DEALER, 'ДД'),
+        (TRADER, 'Торговцы'),
+        (GUILDMASTER, 'Гилдмастеры'),
+        (QUESTGIVER, 'Квестгиверы'),
+        (BLACKSMITH, 'Кузнецы'),
+        (TANNER, 'Кожевники'),
+        (POTION_MAKER, 'Зельевары'),
+        (SPELL_MASTER, 'Мастера Заклинаний')
+    ]
+    name = models.CharField(choices=TYPES)
+    
+    def __str__(self):
+        return self.get_name_display()
+    
+class Response(models.Model):
+    class Meta:
+        verbose_name = 'Отклик'
+        verbose_name_plural = 'Отклики'
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор')
+    post = models.ForeignKey('Post', on_delete=models.CASCADE, verbose_name='id Целевого поста')
+    text = models.TextField()
+    date = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
