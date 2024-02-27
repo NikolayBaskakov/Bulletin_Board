11a56980408c83e3324d6ed409e41aaa2dc59f6d
Nikolay Baskakov <bnn300702@yandex.ru>
2024-02-27 14:27:01 +0300

    update post model

--- Bulletin_board/Main_board/models.py
+++ Bulletin_board/Main_board/models.py
@@ -19,7 +19,7 @@ class Post(models.Model):
     slug = models.SlugField(max_length=20, null=False, unique=True)
     
     def __str__(self):
-        return str(self.pk)
+        return str(self.slug)
     
 class Category(models.Model):
     class Meta:
@@ -58,7 +58,7 @@ class Response(models.Model):
         verbose_name = 'Отклик'
         verbose_name_plural = 'Отклики'
     author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор')
-    post = models.ForeignKey('Post', on_delete=models.CASCADE, verbose_name='id Целевого поста')
+    post = models.ForeignKey('Post', on_delete=models.CASCADE, verbose_name='Целевой пост')
     title = models.CharField(max_length=25, verbose_name='Заголовок')
     text = models.TextField()
     date = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
