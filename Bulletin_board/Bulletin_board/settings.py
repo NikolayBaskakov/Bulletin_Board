c2c81d02e25581bbde8bcc6ffca8c17682e6da11
Nikolay Baskakov <bnn300702@yandex.ru>
2024-02-27 12:51:06 +0300

    add settings for static files

--- Bulletin_board/Bulletin_board/settings.py
+++ Bulletin_board/Bulletin_board/settings.py
@@ -139,7 +139,10 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 AUTH_USER_MODEL = 'Main_board.User'
 
