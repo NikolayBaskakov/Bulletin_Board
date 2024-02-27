3c458cd2ea690acc3af53d716b5a04e9b8d2c38e
Nikolay Baskakov <bnn300702@yandex.ru>
2024-02-28 00:02:07 +0300

    add PostDetail and PostList views and it`s urls adn templates

--- Bulletin_board/Main_board/urls.py
+++ Bulletin_board/Main_board/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import *
 
 urlpatterns = [
+    path('', PostList.as_view(), name='post-list'),
+    path('<slug:slug>/', PostDetail.as_view(), name='post-detail')
 ]
--- Bulletin_board/Main_board/views.py
+++ Bulletin_board/Main_board/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.views.generic import DetailView, ListView
+from .models import *
 # Create your views here.
+class PostList(ListView):
+    model = Post
+    template_name = 'post_list.html'
+    context_object_name = 'posts'
+    
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+    context_object_name = 'post'
