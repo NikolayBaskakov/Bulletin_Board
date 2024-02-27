4525a7640ecdd9e3b55dc8470244d2d6a2ee29f3
Nikolay Baskakov <bnn300702@yandex.ru>
2024-02-27 14:20:09 +0300

    update response model

--- Bulletin_board/Main_board/models.py
+++ Bulletin_board/Main_board/models.py
@@ -59,6 +59,7 @@ class Response(models.Model):
         verbose_name_plural = 'Отклики'
     author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор')
     post = models.ForeignKey('Post', on_delete=models.CASCADE, verbose_name='id Целевого поста')
+    title = models.CharField(max_length=25, verbose_name='Заголовок')
     text = models.TextField()
     date = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     slug = models.SlugField(max_length=20, null=False, unique=True)
