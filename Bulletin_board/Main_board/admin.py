c8d2bb417f897e37fb9f1b3a8ebabde44169c2bb
Nikolay Baskakov <bnn300702@yandex.ru>
2024-02-29 14:31:32 +0300

    override save_model method in PostSummernote class

--- Bulletin_board/Main_board/admin.py
+++ Bulletin_board/Main_board/admin.py
@@ -1,11 +1,17 @@
+from typing import Any
 from django.contrib import admin
 from .models import *
 from django_summernote.admin import SummernoteModelAdmin
 # Register your models here.
 class PostSummernote(SummernoteModelAdmin):
     summernote_fields = ('text',)
+    fields = ['title', 'text', 'category', 'slug']
     list_display = ( 'id', 'slug',  'author', 'category', 'edit_date', 'creation_date')
     prepopulated_fields = {"slug": ("title",)}
+    
+    def save_model(self, request, obj, form, change) -> None:
+        obj.author = request.user
+        return super().save_model(request, obj, form, change)
     
 admin.site.register(Post, PostSummernote)
 
