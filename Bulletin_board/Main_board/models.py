93b8253c0b989aaefb1263125b42262070d432db
Nikolay Baskakov <bnn300702@yandex.ru>
2024-03-01 23:21:18 +0300

    add ACCOUNT_CHANGE_EMAIL, set email for user model required

--- Bulletin_board/Main_board/models.py
+++ Bulletin_board/Main_board/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AbstractUser
 from django.urls import reverse
 # Create your models here.
 class User(AbstractUser):
+    email = models.EmailField(max_length=254, verbose_name='email address')
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
