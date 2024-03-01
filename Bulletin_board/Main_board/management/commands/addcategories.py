6783c39ade5729b512aec994433da1826917f929
Nikolay Baskakov <bnn300702@yandex.ru>
2024-03-01 18:52:52 +0300

    add exception handler to addcategories command

--- Bulletin_board/Main_board/management/commands/addcategories.py
+++ Bulletin_board/Main_board/management/commands/addcategories.py
@@ -1,11 +1,15 @@
 from typing import Any
 from django.core.management.base import BaseCommand
 from Main_board.models import Category
+from django.db.utils import IntegrityError
 
 class Command(BaseCommand):
     help = 'Create all of available categories in Category.TYPES '
     requires_migrations_checks = True
     
     def handle(self, *args, **options):
-        for category in Category.TYPES:
-            Category.objects.create(name=category[0])
+        try:
+            for category in Category.TYPES:
+                Category.objects.create(name=category[0])
+        except IntegrityError:
+            self.stdout.write('Categories allready exixts. News categories haven`t been created.')
